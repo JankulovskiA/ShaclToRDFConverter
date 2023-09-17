427d2eb5eaec55809fbfd0e345b11beda086e0a0
aleks <aleksandar.j516@gmail.com>
2023-09-17 16:32:40 +0200

    first commit

---
+++ src/main/java/com/example/shacltordfconverter/ShaclToRdfConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.shacltordfconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaclToRdfConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShaclToRdfConverterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shacltordfconverter/ShaclToRdfConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shacltordfconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShaclToRdfConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
